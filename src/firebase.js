518c07261998d711920276e2a2fc35fbadbe21d4
Waqas Khalid Obeidy <waqas@siswa.ukm.edu.my>
2020-07-19 14:56:31 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,9 @@
 import firebase from "firebase";
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBm2KlnbUKPS4_9fBzze4AGzh72t1zCkmY",
-    authDomain: "clone-9bbb8.firebaseapp.com",
-    databaseURL: "https://clone-9bbb8.firebaseio.com",
-    projectId: "clone-9bbb8",
-    storageBucket: "clone-9bbb8.appspot.com",
-    messagingSenderId: "615111449751",
-    appId: "1:615111449751:web:fd86153d4b3a518a4a1dd2",
-    measurementId: "G-HSR6ZPK6DV",
+    apiKey: "AIzaSyBm2K.....",
+    authDomain: "clone-9....",
+    databaseURL: "https://c.....",
 
 });
 
